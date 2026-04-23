d343eb13f7843c5ebbd6d0894d5ef084c0dba99a
Arshdeep Singh <arshdeepbansal69@gmail.com>
2026-04-23 20:39:54 +0530

    Fix typo in username 'Anatariksh' to 'Antariksh'

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-username=["Arsh","Amit","Bipan","Daman","Anatariksh","Abhishek"]
+username=["Arsh","Amit","Bipan","Daman","Antariksh","Abhishek"]
 unique_user_ids=[2546077,2546058,2546097,2546104,2546070,2546025]
 unique_pins=[6077,6058,6097,6104,6070,6025]
 balances=[10000,20000,30000,40000,50000,60000]
